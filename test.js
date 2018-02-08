300ead447775be5f7a25c6b33f301ed82fc00ef4
VitaliyGreen <36164866+VitaliyGreen@users.noreply.github.com>
2018-02-08 20:43:53 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,57 @@
+ var myNumberArray = [];
+
+
+createNumber();
+while (compare() == false) {
+	compare();
+}
+console.log('Congratulation! you win! ;-)');
+function createNumber() {
+	var numbArray = 
+	['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];	
+	while (myNumberArray.length < 4) 
+	{
+		var rand = Math.floor(Math.random() * 10);
+		if (numbArray[rand] != (null || undefined)) {
+		myNumberArray.push(numbArray[rand]);
+		numbArray.splice(rand, 1);
+		}
+		else {
+			continue;
+		}
+	}
+	myNumberArray = myNumberArray.join('');
+}
+
+function compare() {
+var check = false;
+var bull = 0, cow = 0;
+while (check != false;) {
+const readlineSync = require('readline-sync');
+var custArray = readlineSync.question('Enter your number please \n');
+if(custArray.length < 4){
+	console.log('Too short');
+}
+else if (custArray.length > 4){
+	console.log('Too long');
+}
+else{
+	for (var i = 0; i < myNumberArray.length; ++i) {
+		
+		if (myNumberArray.charAt(i) == custArray.charAt(i)) {
+			bull++;
+			continue;
+		}
+		if (myNumberArray.indexOf(custArray.charAt(i)) != -1) {
+			cow++;
+		}
+	}
+	console.log(bull + 'bulls, ' + cow + 'cows');
+	if (bull == 4) {
+		check = true;
+
+	}
+	}
+}
+	return check;
+}
